190509135f34a2f611efc2abcee707960bb97d48
netojso <neto.devx@gmail.com>
2025-02-24 10:12:13 -0300

    feat: add cors

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/netojso/elephrases-api/config"
 	_ "github.com/netojso/elephrases-api/docs"
@@ -33,6 +36,17 @@ func main() {
 	defer config.ClosePostgresDBConnection(db)
 
 	gin := gin.Default()
+
+	corsConfig := cors.DefaultConfig()
+
+	corsConfig.AllowAllOrigins = true
+	corsConfig.AllowMethods = []string{"POST", "GET", "PUT", "OPTIONS"}
+	corsConfig.AllowHeaders = []string{"Origin", "Content-Type", "Authorization", "Accept", "User-Agent", "Cache-Control", "Pragma"}
+	corsConfig.ExposeHeaders = []string{"Content-Length"}
+	corsConfig.AllowCredentials = true
+	corsConfig.MaxAge = 12 * time.Hour
+
+	gin.Use(cors.New(corsConfig))
 
 	gin.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
--- go.mod
+++ go.mod
@@ -40,6 +40,7 @@ require (
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/gin-contrib/cors v1.7.3 // indirect
 	github.com/gin-contrib/sse v1.0.0 // indirect
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
 	github.com/go-openapi/jsonreference v0.21.0 // indirect
